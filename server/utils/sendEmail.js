bcfd513537170485a0db7f4b07673a5f34332d5c
Sundhar Karthick <112369094+Sundharkarthick11@users.noreply.github.com>
2026-08-29 19:20:37 +0530

    SMTP verify test

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -17,6 +17,13 @@ const transporter = nodemailer.createTransport({
   },
 });
 
+transporter.verify((error, success) => {
+  if (error) {
+    console.log("SMTP VERIFY ERROR:", error);
+  } else {
+    console.log("SMTP SERVER READY");
+  }
+});
 const sendEmail = async ({ to, subject, html }) => {
   try {
     const info = await transporter.sendMail({
